a4f348dd871ccf7a87f1a2e2909de7190bc4ab78
saudkhanbpk <saudkhanbpk@gmail.com>
2025-06-30 11:01:08 +0500

    update the environment varailes

--- src/index.js
+++ src/index.js
@@ -10,13 +10,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// app.use(cors({
-//   origin: [
-//     'http://localhost:5173'
-//   ],
-//   credentials: true
-// }));
-app.use(cors());
+app.use(cors({
+  origin: [
+    process.env.Vite_CLIENT_URL
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/leads", leadRoutes);
